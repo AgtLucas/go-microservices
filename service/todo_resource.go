bd6344173d9bea18ef6d25d7810781aa5cb5622d
Lucas <lucas@agtlucas.com>
2015-04-12 18:01:57 -0300

    Update todo_resource

--- service/todo_resource.go
+++ service/todo_resource.go
@@ -70,3 +70,60 @@ func (tr *TodoResource) UpdateTodo(c *gin.Context) {
     c.JSON(200, todo)
   }
 }
+
+func (tr *TodoResource) PatchTodo(c *gin.Context) {
+  id, err := tr.getId(c)
+  if err != nil {
+    c.JSON(400, api.NewError("Problem decoding id sent"))
+    return
+  }
+
+  var json []api.Patch
+  defer func() {
+    if r := recover(); r != nil {
+      if json[0].0p != "replace" && json[0].Path != "/status" {
+        c.JSON(400, api.NewError("PATCH support is limited and can only replace the /status path"))
+        return
+      }
+
+      var todo api.Todo
+
+      if tr.db.First(&todo, id).RecordNotFound() {
+        c.JSON(404, api.NewError("not found"))
+      } else {
+        todo.Status = json[0].Value
+
+        tr.db.Save(&todo)
+        c.JSON(200, todo)
+      }
+    }
+  }()
+  c.Bind(&json)
+}
+
+func (tr *TodoResource) DeleteTodo(c *gin.Context) {
+  id, err := tr.getId(c)
+  if err != nil {
+    c.JSON(400, api.NewError("Problem decoding id sent"))
+    return
+  }
+
+  var todo api.Todo
+
+  if tr.db.First(&todo, id).RecordNotFound() {
+    c.JSON(404, api.NewError("not found"))
+  } else {
+    tr.db.Delete(&todo)
+    c.Data(204, "application/json", make([]byte, 0))
+  }
+}
+
+func (tr *TodoResource) getId(c *gin.Context) (int32, error) {
+  idStr := c.Params.ByName("id")
+  id, err := strconv.Atoi(idStr)
+  if err != nil {
+    log.Print(err)
+    return 0, err
+  }
+  return int32(id), nil
+}
